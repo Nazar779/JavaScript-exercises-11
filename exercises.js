df1d3e28ea0e418267f14a61e113046b8b1e314b
Nazar779 <nazarhorkasov965@gmail.com>
2025-11-18 20:05:56 +0300

    Create exercises.js
    this exercise Javascript is normal

---
+++ exercises.js
@@ -0,0 +1,16 @@
+// write a function that converts minutes to seconds
+function convertMinutesInSeconds(minutes){
+    return 60 * parseInt(minutes)
+}
+console.log(convertMinutesInSeconds(2))
+// write function that find area of triangle formula area of triangle (base / height) / 2
+function AreaOfTriangle(base, height){
+    return(base * height) / 2
+}
+console.log(AreaOfTriangle(10, 6))
+// create function that convert age to  seconds ignore leap year
+function convertAgetoDays(age){
+    return 365 * age
+}
+console.log(convertAgetoDays(20))
+// Sorry guys, write your feedback if you like it, I'll add the code if not, write what needs to be improved
